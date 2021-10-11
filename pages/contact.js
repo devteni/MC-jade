80de29059e39d74c218d21c0f92e093479d16d9b
devteni <fatunmbiteniola001@gmail.com>
2021-10-11 18:11:07 +0100

    chore: fixed ui responsiveness on mobile

--- pages/contact.js
+++ pages/contact.js
@@ -6,7 +6,13 @@ import { Container, Row, Col, Button } from 'react-bootstrap';
 import { Footer } from '../components/Footer';
 
 export default function contact() {
-    const [status, setStatus] = useState("Sending");
+    const [status, setStatus] = useState("Send");
+    const handleSubmit = e => {
+        e.preventDefault();
+        setStatus("Sending");
+        setStatus("Sent!")
+        setStatus("Send")
+    }
     return (
         <div>
             <NavBar />
@@ -14,37 +20,65 @@ export default function contact() {
                 <Container>
                     <Row>
                         <Col sm>
-                            <h1 className="p-5 font-weight-bolder text-center herotext">MAKE YOUR ENQUIRIES</h1>
+                            <h1 className="p-5 font-weight text-center herotext">MAKE YOUR ENQUIRIES</h1>
                         </Col>
                     </Row>
                 </Container>
             </div>
-            <div className="form">
-               <Container className="text-white">
+            <div style={{padding: "5px"}} className="form" >
+               <div className="text-white">
                    <Row>
-                    <Col lg={6}>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Label>Name*</Form.Label>
-                                <Form.Control type="email" placeholder="name@example.com" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Label>Email address*</Form.Label>
-                                <Form.Control type="email" placeholder="email@example.com" />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                <Form.Label>Message*</Form.Label>
-                                <Form.Control as="textarea" rows={3} />
-                            </Form.Group>
-                            <button className="cta px-4 py-2">Send</button>
-                        </Form>
+                        <Col className="col-sm-6 p-5">
+                            <form id="contact-form" name="contact-form" action="mail.php" method="POST">
+                                <div class="row">
+                                    <div class="col-sm-6">
+                                        <div class="md-form mb-0">
+                                            <label for="name" style={{fontSize: "small"}}>Your name</label>
+                                            <input type="text" id="name" name="name" class="form-control"/> 
+                                        </div>
+                                    </div>
+                                    
+                                    <div class="col-sm-6">
+                                        <div class="md-form mb-0">
+                                            <label for="email" style={{fontSize: "small"}}>Your email</label>
+                                            <input type="text" id="email" name="email" class="form-control"/>
+                                            
+                                        </div>
+                                    </div>
+                                </div>
+                            
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <div class="md-form mb-0">
+                                            <label for="subject" style={{fontSize: "small"}}>Subject</label>
+                                            <input type="text" id="subject" name="subject" class="form-control"/>  
+                                        </div>
+                                    </div>
+                                </div>
+                            
+                                <div class="row">
+
+                                    {/* <!--Grid column--> */}
+                                    <div class="col-sm-12">
+
+                                        <div class="md-form">
+                                            <label for="message" style={{fontSize: "small"}}>Your message</label>
+                                            <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>    
+                                        </div>
+                                    </div>
+                                </div>
+                                {
+                                    <button className="cta px-4 py-2 my-4 text-center" onClick={handleSubmit}>{ status }</button>
+                                }
+                            </form>
                         </Col>
-                        <Col className="mx-5">
-                            <h1 className="p-4">Want to discuss your next project?</h1>
+                        <Col sm={4} className="text-center p-3">
+                            <h1 className="p-2 m-4">Want to discuss your next project?</h1>
+                            <br />
                             <span class="cta">Contact Sales team</span>
                         </Col>
                     </Row>
-            </Container>
+            </div>
                    
                 
             </div>
