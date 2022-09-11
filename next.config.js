6673c08af411676ad5c852bf2731213f656d743b
Teniola Fatunmbi <70762806+devteni@users.noreply.github.com>
2022-09-11 20:57:56 +0100

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  assetPrefix: isProd ? '/your-github-repo-name/' : '',
+  assetPrefix: isProd ? '/mcjade/' : '',
   reactStrictMode: true,
 }
